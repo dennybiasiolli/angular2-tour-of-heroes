5400fca101d6d08fa203fa62afd71eabaaf974b8
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2015-11-25 11:47:49 +0100

    Editing Our Hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
     selector: 'my-app',
@@ -6,8 +6,12 @@ import {bootstrap, Component} from 'angular2/angular2';
         <h1>{{title}}</h1>
         <h2>{{hero.name}} details!</h2>
         <div><label>id: </label>{{hero.id}}</div>
-        <div><label>name: </label>{{hero.name}}</div>
-        `
+        <div>
+            <label>name: </label>
+            <div><input [(ng-model)]="hero.name" placeholder="name"></div>
+        </div>
+        `,
+    directives: [FORM_DIRECTIVES]
 })
 
 class AppComponent {
