b89974d0c0e50c53460207a7fb1b33e197fce20e
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2015-11-25 11:42:34 +0100

    Hero object, Adding more HTML, Multi-line template strings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,26 @@
 import {bootstrap, Component} from 'angular2/angular2';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div><label>name: </label>{{hero.name}}</div>
+        `
 })
-class AppComponent { }
+
+class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+class Hero {
+    id: number;
+    name: string;
+}
+
 bootstrap(AppComponent);
