166a00eeee63fa101b2437dbb75327f3cd632016
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2015-11-25 12:18:46 +0100

    Added external style

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,23 +7,8 @@ class Hero {
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/index.tpl.html',
-    styles: [`
-        .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
-        .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
-        .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
-        .heroes .badge {
-            font-size: small;
-            color: white;
-            padding: 0.1em 0.7em;
-            background-color: #369;
-            line-height: 1em;
-            position: relative;
-            left: -1px;
-            top: -1px;
-        }
-        .selected { background-color: #EEE; color: #369; }
-    `],
+    templateUrl: 'app/heroes.tpl.html',
+    styleUrls: ['app/heroes.css'],
     directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 
