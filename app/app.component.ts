f594738d47225d7b4264afbd433294c7735b3299
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2015-12-17 10:15:19 +0100

    Completed tutorial#3 in angular2beta0 and created template files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {HeroDetailComponent} from './hero-detail.component';
 
 interface Hero {
     id: number;
@@ -7,41 +8,9 @@ interface Hero {
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <h2>My Heroes</h2>
-        <ul class="heroes">
-            <li *ngFor="#hero of heroes"
-                [class.selected]="hero === selectedHero"
-                (click)="onSelect(hero)">
-                <span class="badge">{{hero.id}}</span> {{hero.name}}
-            </li>
-        </ul>
-        <div *ngIf="selectedHero">
-            <h2>{{selectedHero.name}} details!</h2>
-            <div><label>id: </label>{{selectedHero.id}}</div>
-            <div>
-                <label>name: </label>
-                <div><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
-            </div>
-        </div>
-    `,
-    styles: [`
-        .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
-        .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
-        .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
-        .heroes .badge {
-            font-size: small;
-            color: white;
-            padding: 0.1em 0.7em;
-            background-color: #369;
-            line-height: 1em;
-            position: relative;
-            left: -1px;
-            top: -1px;
-        }
-        .selected { background-color: #EEE; color: #369; }
-    `]
+    templateUrl: 'app/app.template.html',
+    styleUrls: ['app/app.styles.css'],
+    directives: [HeroDetailComponent]
 })
 export class AppComponent {
     public title = 'Tour of Heroes';
