2d6a9ef814c3b28ae91ed8a22d3b6d522890c939
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2016-02-25 16:15:29 +0100

    Tutorial #2 - Master/Detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,20 +7,25 @@ interface Hero {
 
 @Component({
   selector: 'my-app',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <div><input [(ngModel)]="hero.name" placeholder="name"></div>
-  </div>
-  `
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
   public title = 'Tour of Heroes';
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+  public heroes = HEROES;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
