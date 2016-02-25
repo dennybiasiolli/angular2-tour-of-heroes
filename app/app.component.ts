89728b463dfdab9a6d6f6078f55552e3e3ad8a98
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-02-25 12:31:58 +0100

    Reset to initial status of tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,33 +1,7 @@
 import {Component} from 'angular2/core';
-import {HeroDetailComponent} from './hero-detail.component';
-
-interface Hero {
-    id: number;
-    name: string;
-}
 
 @Component({
-    selector: 'my-app',
-    templateUrl: 'app/app.template.html',
-    styleUrls: ['app/app.styles.css'],
-    directives: [HeroDetailComponent]
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent {
-    public title = 'Tour of Heroes';
-    public selectedHero: Hero;
-    public heroes = HEROES;
-    onSelect(hero: Hero) { this.selectedHero = hero; }
-}
-
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
-];
+export class AppComponent { }
